acf95df7070d885c91ae1337544922d892b7d6b5
Sahil-Popat-Potale <sahilpopat97@gmail.com>
2025-10-26 00:31:16 +0530

    implementing SCSS technique

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import App from './App';
 import { createRoot } from 'react-dom/client'; 
 import { BrowserRouter } from 'react-router-dom'; 
 import { AuthProvider } from './AuthContext';
-//import './styles.css';
+//import './styles/main.scss';
 
 createRoot(document.getElementById('root')).render(
     <React.StrictMode>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({ 
     plugins:[react()], 
-    server:{ port:5173 } 
+    server:{ port:5173 },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "./src/styles/scss/_variables.scss";`
+        }
+      }
+    }
 });
